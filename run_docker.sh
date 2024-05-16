b8ff640bf624a6fb9a87b6c1bd82ce0c663a2f1c
Dominik Meurer <50107525+DMeurer@users.noreply.github.com>
2024-05-16 22:52:09 +0200

    oopsies

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,7 @@
 git pull --force
 
 # rebuilding the docker compose, with zero downtime
-docker-compose up -d --no-deps --build
+docker compose up -d --no-deps --build
 
 # remove the old images
 docker image prune -f
