9add78339435041fb6da34dc60dca59db0f16297
Dominik Meurer <50107525+DMeurer@users.noreply.github.com>
2024-05-16 23:19:51 +0200

    add output

--- deployment/cronjob.sh
+++ deployment/cronjob.sh
@@ -2,4 +2,4 @@ cd ../
 
 LOCK_FILE="$(pwd)/deployment/cronjob.lock"
 
-flock -n "$LOCK_FILE" ./deployment/check-updates.sh
+flock -n "$LOCK_FILE" ./deployment/check-updates.sh >> ./deployment/logs/cronjob.log 2>&1
