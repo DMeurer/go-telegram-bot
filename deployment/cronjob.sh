e602bb579261e00c0b3758787489523b4bf8f9d8
DMeurer <dom.meurer@gmx.de>
2024-05-18 12:35:55 +0200

    fiddeling

--- deployment/cronjob.sh
+++ deployment/cronjob.sh
@@ -1,7 +1,25 @@
-cd /home/raspi/go-telegram-bot || exit
+echo "Starting" >> "deployment/logs/cronjob.log" 2>&1
+
+cd /home/raspi/go-telegram-bot
+
+pwd  >> "deployment/logs/cronjob.log" 2>&1
+
+echo "Switched" >> "deployment/logs/cronjob.log" 2>&1
+
+echo "$(date --utc): Starting Cron job..." >> "deployment/logs/cronjob.log" 2>&1
+
+echo "written" >> "deployment/logs/cronjob.log" 2>&1
 
 mkdir -p deployment/logs
 
-LOCK_FILE="$(pwd)/deployment/cronjob.lock"
+echo "made dir" >> "deployment/logs/cronjob.log" 2>&1
 
-flock -n "$LOCK_FILE" "$(pwd)/deployment/check-updates.sh" >> "$(pwd)/deployment/logs/cronjob.log" 2>&1
+LOCK_FILE="$(pwd)/deployment/cronjob.lock" >> "deployment/logs/cronjob.log" 2>&1
+
+echo "define lockfile" >> "deployment/logs/cronjob.log" 2>&1
+
+sudo flock -n "$LOCK_FILE" "deployment/check-updates.sh" >> "deployment/logs/cronjob.log" 2>&1
+
+echo "done" >> "deployment/logs/cronjob.log" 2>&1
+
+ls deployment/logs/
