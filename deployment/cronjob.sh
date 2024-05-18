11c5bca8111e6a0b18323e2fb1564c928fa3dd4a
Dominik Meurer <dom.meurer@gmx.de>
2024-05-18 15:59:43 +0200

    try for fun

--- deployment/cronjob.sh
+++ deployment/cronjob.sh
@@ -1,9 +1,5 @@
 cd /home/raspi/go-telegram-bot || echo "$(date --utc): Failed to cd into  /home/raspi/go-telegram-bot aborting..." >> "deployment/logs/cronjob.log" 2>&1
 
-pwd
-
-pwd  >> "deployment/logs/cronjob.log" 2>&1
-
 echo "$(date --utc): Starting Cron job..." >> "deployment/logs/cronjob.log" 2>&1
 
 mkdir -p deployment/logs
--- deployment/start-scronjob.sh
+++ deployment/start-scronjob.sh
@@ -1,1 +1,4 @@
 crontab -u raspi -e
+
+# my configuration
+# * * * * * sh /home/raspi/go-telegram-bot/deployment/cronjob.sh
