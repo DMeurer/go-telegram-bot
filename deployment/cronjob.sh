58f82b8e8bca836e9bb14ddd7b9cbee429c5534c
DMeurer <dom.meurer@gmx.de>
2024-05-18 15:57:07 +0200

    It works

--- deployment/cronjob.sh
+++ deployment/cronjob.sh
@@ -1,4 +1,6 @@
-cd /home/raspi/go-telegram-bot || echo "$(date --utc): Failed to cd into  /home/raspi/go-telegram-bot aborting..." >> "deployment/logs/cronjob.log" 2>&1 && exit 1
+cd /home/raspi/go-telegram-bot || echo "$(date --utc): Failed to cd into  /home/raspi/go-telegram-bot aborting..." >> "deployment/logs/cronjob.log" 2>&1
+
+pwd
 
 pwd  >> "deployment/logs/cronjob.log" 2>&1
 
@@ -8,4 +10,4 @@ mkdir -p deployment/logs
 
 LOCK_FILE="/tmp/go-telegram-bot.lockfile"
 
-flock -n "$LOCK_FILE" deployment/check-updates.sh >> deployment/logs/cronjob.log 2>&1
+flock -n "$LOCK_FILE" deployment/check-updates.sh >> "deployment/logs/cronjob.log" 2>&1
