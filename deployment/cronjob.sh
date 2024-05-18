cca57b30eeef986b175e5718a88d8c7b6da21bba
Dominik Meurer <dom.meurer@gmx.de>
2024-05-18 12:39:13 +0200

    maybe it works now

--- deployment/cronjob.sh
+++ deployment/cronjob.sh
@@ -1,25 +1,11 @@
-echo "Starting" >> "deployment/logs/cronjob.log" 2>&1
-
-cd /home/raspi/go-telegram-bot
+cd /home/raspi/go-telegram-bot || echo "$(date --utc): Failed to cd into  /home/raspi/go-telegram-bot aborting..." >> "deployment/logs/cronjob.log" 2>&1 && exit 1
 
 pwd  >> "deployment/logs/cronjob.log" 2>&1
 
-echo "Switched" >> "deployment/logs/cronjob.log" 2>&1
-
 echo "$(date --utc): Starting Cron job..." >> "deployment/logs/cronjob.log" 2>&1
-
-echo "written" >> "deployment/logs/cronjob.log" 2>&1
 
 mkdir -p deployment/logs
 
-echo "made dir" >> "deployment/logs/cronjob.log" 2>&1
+LOCK_FILE="$(pwd)/deployment/cronjob.lock"
 
-LOCK_FILE="$(pwd)/deployment/cronjob.lock" >> "deployment/logs/cronjob.log" 2>&1
-
-echo "define lockfile" >> "deployment/logs/cronjob.log" 2>&1
-
-sudo flock -n "$LOCK_FILE" "deployment/check-updates.sh" >> "deployment/logs/cronjob.log" 2>&1
-
-echo "done" >> "deployment/logs/cronjob.log" 2>&1
-
-ls deployment/logs/
+flock -n "$LOCK_FILE" "deployment/check-updates.sh" >> "deployment/logs/cronjob.log" 2>&1
